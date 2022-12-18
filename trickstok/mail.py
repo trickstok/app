e30f0027ebc1de2a05635645486f7256bc18f871
CAMARM_Flipz <armand.camponovo@icloud.com>
2022-12-18 11:41:51 +0100

    mail.py

---
+++ trickstok/mail.py
@@ -0,0 +1,60 @@
+import smtplib
+from email.mime.text import MIMEText
+
+from trickstok import DatabaseObject
+
+
+class Template:
+
+    base = """
+    <body>
+    Bonjour {email} 👋 !
+    {content}
+    Bons tricks,<br>
+    L'équipe TricksTok.<br>
+    </body>
+    """
+
+    betaopen = """
+    Enfin, la beta access de TricksTok est là 🤗 ! Tu peux dès maintenant te rendre sur <a href={link}>ce lien</a> pour te créer un compte TricksTok 🤟 ! (Attention ce lien est unique, ne le passe à personne !)<br>
+    Nous le savons TricksTok doit être truffé de bug et erreurs, à toi d'en reporter le plus possible sur <a href="http://support.camarm.fr/newissue">support.camarm.fr/newissue</a> en prenant soin de détailler le problème, ou est-il apparu et quand. (Les 10 avec le plus de report auront une petite surprise 🤫)<br>
+    Bonne beta-access,<br>
+    """
+
+    betalog = """
+    Tout d’abord merci de t’être inscris à la beta access 🙏. Le 1er janvier (normalement 😅) tu recevra un email contenant tous ce dont tu aura besoin pour accéder et mener à bien ta première expérience sur TricksTok en beta access.<br>
+    En attendant, tu peux nous visiter de temps en temps sur YouTube (https://youtube.com/@trickstok) pour savoir où on en est 😊 !<br>
+    """
+
+    banned = """
+    Bonjour.
+    Suite à un non respect du règlement tu as été banni pour la raison suivante:<br>
+    <b>{reason}</b>
+    <br>
+    Ton bannissement est {type}{end}<br> 
+    L'équipe TricksTok.
+    """
+
+
+class Mailer(DatabaseObject):
+
+    def __init__(self, user, password, url):
+        super().__init__(user, password, url)
+        self.mails = self.collection.mailing
+        self._from = 'Trickstok <trickstok@camarm.dev>'
+
+    def add_to_list(self, email, list):
+        self.mails.insert_one({'mail': email, 'list': list})
+
+    def get_list_mails(self, list):
+        self.mails.find({'list': list})
+
+    def send_mail(self, to, subject, content):
+        server = smtplib.SMTP('192.168.1.141', 25)
+        message = MIMEText(content, 'html')
+        message['From'] = self._from
+        message['Subject'] = subject
+        message['To'] = to
+        server.send_message(message, 'armand@camponovo.xyz', to)
+
+
